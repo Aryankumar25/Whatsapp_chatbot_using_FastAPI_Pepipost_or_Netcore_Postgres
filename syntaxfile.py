d7f4d04ae1c97df90d2f0fbef0327469df8eccec
Aryan Kumar <101729054+Aryankumar25@users.noreply.github.com>
2025-06-23 15:08:48 +0530

    syntax for incoming data has been updated (for message-type: text)

---
+++ syntaxfile.py
@@ -0,0 +1,20 @@
+{
+    'incoming_message': [
+        {
+            'context':
+            {
+                'message_id': '',
+                'ncmessage_id': ''
+            },     
+            'from': '91XXXXXXXXXXX',
+            'from_name': 'Aryan', 
+            'message_id': 'wamid.XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX', 
+            'message_type': 'TEXT',
+            'received_at': '1750238660', 
+            'text_type': {
+                'text': 'Ok'
+            },
+            'to': '91XXXXXXXXXXX'
+        }
+    ]
+}
